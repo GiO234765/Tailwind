20966493841b175fa448483f4a0d7ee3cee50db1
GiO234765 <brokaknj@gmail.com>
2026-03-20 09:21:30 +0700

    update

--- Project/tea.java
+++
@@ -1,9 +0,0 @@
-import  java.util.Scanner;
-class tea{
-    public static void main(String[] args){
-        Scanner cin = new Scanner(System.in);
-        String username;
-        System.out.print(" Enter Your Username: ");
-        username=cin.nextLine();
-    }
-}
